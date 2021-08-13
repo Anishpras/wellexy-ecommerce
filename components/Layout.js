faed1a9784ef244feb193c79a617c62a2b80549d
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-08-13 20:02:37 +0530

    Links added

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,12 @@
-import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import Head from "next/head";
+import NextLink from "next/link";
 import useStyles from "../utils/styles";
 export default function Layout({ children }) {
   const classes = useStyles();
@@ -11,7 +18,20 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Wellexy</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Wellexy</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
