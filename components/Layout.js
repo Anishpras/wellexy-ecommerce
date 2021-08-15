55c9a8a85016af991c260ac6b0d2fac91c8fb07a
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-08-15 15:30:54 +0530

    Product page and MuiTheme added

--- components/Layout.js
+++ components/Layout.js
@@ -3,41 +3,72 @@ import {
   Container,
   Link,
   Toolbar,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
   Typography,
 } from "@material-ui/core";
 import Head from "next/head";
 import NextLink from "next/link";
 import useStyles from "../utils/styles";
-export default function Layout({ children }) {
+export default function Layout({ title, description, children }) {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
       {" "}
       <Head>
-        <title>Wellexy</title>
+        <title>{title ? `${title} - Wellexy` : "Wellexy"}</title>
+        {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>Wellexy</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Wellexy PVT LTD </Typography>
-      </footer>
+
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Next Amazona.</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
